ab03cca7f55bae5a766a7f7b447c035359c71866
kongjiadongyuan <zhaggbl@foxmail.com>
2022-04-21 07:20:30 +0000

    change log format

--- compile_manager.py
+++ compile_manager.py
@@ -14,10 +14,7 @@ COMPILE_SCRIPT = "/root/compile.sh"
 
 
 
-def do_compile(package_name, path):
-    logger = logging.getLogger(package_name)
-    logger.setLevel(logging.DEBUG)
-    logger.info(f"Start compiling package {package_name}")
+def do_compile(package_name, path, logger):
     save_path = os.path.join(path, package_name)
     logger.debug(f"Checking if path {save_path} exists")
     assert (not os.path.exists(save_path))
@@ -39,16 +36,15 @@ def do_compile(package_name, path):
 
 
 
-def compile_package(package_name, path):
-    logger = logging.getLogger(__file__)
-    logger.setLevel(logging.DEBUG)
+def compile_package(package_name, path, logger):
     status = package_status(package_name, path)
     if status == PackageStatus.NOT_STARTED:
-        do_compile(package_name, path)
+        do_compile(package_name, path, logger)
     elif status == PackageStatus.EXIT_EARLY:
         logger.info(f"{package_name} exited early, remove and recompile")
         shutil.rmtree(os.path.join(path, package_name))
-        do_compile(package_name, path)
+        logger.info(f"{package_name} remove ok")
+        do_compile(package_name, path, logger)
     elif status == PackageStatus.DONE or status == PackageStatus.ERROR:
         logger.info(f"{package_name} already compiled, skip")
     else:
@@ -65,10 +61,13 @@ def compile_list_serially(json_path, path):
     logger.debug(f"{len(package_list)} packages to compile")
     for package in package_list:
         package_name = package['package']
-        compile_package(package_name, path)
+        try:
+            compile_package(package_name, path, logger)
+        except Exception as e:
+            logger.error(f"{e}")
 
 def compile_list_worker(queue, path, idx):
-    logger = logging.getLogger(__file__)
+    logger = logging.getLogger(f"worker{idx}")
     logger.setLevel(logging.DEBUG)
     logger.info(f"Start worker {idx}")
     while True:
@@ -78,7 +77,7 @@ def compile_list_worker(queue, path, idx):
             logger.info(f"Worker {idx} meet queue empty, sleep")
             time.sleep(2)
         logger.info(f"Worker {idx} fetching package {package_name}, begin compile")
-        compile_package(package_name, path)
+        compile_package(package_name, path, logger)
 
 def compile_list_parallel(json_path, path, max_worker=10):
     logger = logging.getLogger(__file__)
@@ -94,13 +93,20 @@ def compile_list_parallel(json_path, path, max_worker=10):
     pool = multiprocessing.Pool(max_worker)
     for idx in range(max_worker):
         pool.apply_async(compile_list_worker, (queue, path, idx))
+    
+    wait_count = 0
     for package in package_list:
         package_name = package['package']
         while True:
             qsize = queue.qsize()
             if qsize > max_worker * 2:
-                time.sleep(10)
+                if wait_count < 10:
+                    time.sleep(0.1)
+                    wait_count += 1
+                else:
+                    time.sleep(10)
             else:
+                wait_count = 0
                 logger.info(f"Queue size {qsize}, put package {package_name}")
                 queue.put(package_name)
                 break
@@ -115,7 +121,7 @@ def clear_container():
     client = docker.from_env()
     for container in client.containers.list(all=True):
         if container.image.tags[0] == IMAGE:
-            logger.info(f"Removing container {container.id}")
+            print(f"Removing container {container.id}")
             container.remove(force=True)
 
 if __name__ == '__main__':
